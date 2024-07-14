6e024ad5ec64c6405d161a1eb2cfba8449c3dda6
Murugesh <murugeshvj@gmail.com>
2024-07-14 11:06:55 +0530

    redux is added

--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -1,12 +1,30 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { useLocation, useParams } from "react-router-dom";
 
 const ProductDetails = () => {
   const location = useLocation();
   const params = useParams();
+
+  const [prductInfo, setProductInfo] = useState({});
   console.log(location, "this is locattion");
   console.log(params, "this is params");
+  const getProductInfo = async () => {
+    try {
+      const res = await axios(`https://dummyjson.com/products/${params.id}`);
+      if (res.status === 200) {
+        setProductInfo(res.data);
+      }
+    } catch (error) {
+      console.log(error, "error");
+    }
+  };
 
+  useEffect(() => {
+    if (params?.id) {
+      getProductInfo();
+    }
+  }, [params.id]);
   return <div>ProductDetails</div>;
 };
 
