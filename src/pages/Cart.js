f88007cff46a6ce7f59b20999a86ff8a0dba0f71
sachin ilr <sachu.ilrad@gmail.com>
2024-08-04 01:48:33 +0530

    cart upgrade

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,16 +1,23 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-import Grid from "@mui/material/Grid";
+import {
+  Typography,
+  Button,
+  Grid,
+  Paper,
+  Box,
+  IconButton,
+  Divider,
+  Container,
+} from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 import RemoveIcon from "@mui/icons-material/Remove";
+import DeleteIcon from "@mui/icons-material/Delete";
 import {
   removeFromCart,
   incrementCart,
   decrementCart,
 } from "../redux/reducer/products";
-import Box from "@mui/material/Box";
 
 const Cart = () => {
   const cart = useSelector((state) => state.products.cart);
@@ -37,63 +44,93 @@ const Cart = () => {
   };
 
   return (
-    <Box sx={{ padding: 2 }}>
+    <Container maxWidth="lg" sx={{ mt: 4 }}>
       <Typography variant="h4" gutterBottom>
-        Cart
+        Your Shopping Cart
       </Typography>
       {cart.length === 0 ? (
         <Typography variant="body1">Your cart is empty.</Typography>
       ) : (
         <>
-          {cart.map((item) => (
-            <Grid container spacing={2} key={item.id} alignItems="center">
-              <Grid item xs={12} sm={6}>
-                <Typography variant="h6">{item.title}</Typography>
-                <Typography variant="body2">Price: ${item.price}</Typography>
-              </Grid>
-              <Grid item xs={12} sm={6} container alignItems="center">
-                <Typography variant="body2" gutterBottom>
-                  Quantity:
-                </Typography>
-                <Button
-                  variant="outlined"
-                  size="small"
-                  onClick={() => handleDecrementCart(item)}
-                  sx={{ minWidth: "30px", padding: "0 8px" }}
-                >
-                  <RemoveIcon />
-                </Button>
-                <Typography variant="body2" sx={{ padding: "0 8px" }}>
-                  {item.quantity}
-                </Typography>
-                <Button
-                  variant="outlined"
-                  size="small"
-                  onClick={() => dispatch(incrementCart(item))}
-                  sx={{ minWidth: "30px", padding: "0 8px" }}
-                >
-                  <AddIcon />
-                </Button>
-              </Grid>
-            </Grid>
-          ))}
-          <Typography variant="h6" gutterBottom>
-            Total Price: ${totalPrice.toFixed(2)}
-          </Typography>
-          <Button variant="contained" color="primary" sx={{ marginTop: 2 }}>
-            Checkout
-          </Button>
-          <Button
-            variant="contained"
-            color="secondary"
-            sx={{ marginTop: 2, marginLeft: 2 }}
-            onClick={handleRemoveAll}
-          >
-            Remove All
-          </Button>
+          <Paper elevation={3} sx={{ p: 2, mb: 2 }}>
+            {cart.map((item) => (
+              <React.Fragment key={item.id}>
+                <Grid container spacing={2} alignItems="center">
+                  <Grid item xs={12} sm={3}>
+                    <Box
+                      component="img"
+                      sx={{
+                        width: "100%",
+                        height: 100,
+                        objectFit: "contain",
+                      }}
+                      alt={item.title}
+                      src={item.thumbnail}
+                    />
+                  </Grid>
+                  <Grid item xs={12} sm={4}>
+                    <Typography variant="subtitle1">{item.title}</Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      ${item.price}
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={12} sm={3}>
+                    <Box display="flex" alignItems="center">
+                      <IconButton
+                        size="small"
+                        onClick={() => handleDecrementCart(item)}
+                      >
+                        <RemoveIcon />
+                      </IconButton>
+                      <Typography sx={{ mx: 1 }}>{item.quantity}</Typography>
+                      <IconButton
+                        size="small"
+                        onClick={() => dispatch(incrementCart(item))}
+                      >
+                        <AddIcon />
+                      </IconButton>
+                    </Box>
+                  </Grid>
+                  <Grid item xs={12} sm={2}>
+                    <Typography variant="subtitle1">
+                      ${(item.price * item.quantity).toFixed(2)}
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Box display="flex" justifyContent="flex-end">
+                      <IconButton
+                        color="error"
+                        onClick={() => handleRemoveFromCart(item)}
+                      >
+                        <DeleteIcon />
+                      </IconButton>
+                    </Box>
+                  </Grid>
+                </Grid>
+                <Divider sx={{ my: 2 }} />
+              </React.Fragment>
+            ))}
+            <Box sx={{ display: "flex", justifyContent: "flex-end", mt: 2 }}>
+              <Typography variant="h6">
+                Total: ${totalPrice.toFixed(2)}
+              </Typography>
+            </Box>
+          </Paper>
+          <Box sx={{ display: "flex", justifyContent: "flex-end", mt: 2 }}>
+            <Button variant="contained" color="primary" sx={{ mr: 2 }}>
+              Checkout
+            </Button>
+            <Button
+              variant="outlined"
+              color="secondary"
+              onClick={handleRemoveAll}
+            >
+              Remove All
+            </Button>
+          </Box>
         </>
       )}
-    </Box>
+    </Container>
   );
 };
 
